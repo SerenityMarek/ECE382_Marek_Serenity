8f29bb4c4ae57d9780999bb333ddc9efd9886b1a
SerenityMarek <c28serenity.marek@afacademy.af.edu>
2026-08-25 19:29:29 -0600

    Lab4!

--- Lab04_Cryptography/cryptography.asm
+++ Lab04_Cryptography/cryptography.asm
@@ -58,20 +58,27 @@ EOM    .byte  '#'
 ; Return:
 ;   None
 Encrypt:   .asmfunc
-; ============ Add your code and comments below ===================
+; ============ Add your code and comments below =================== ; Use EOM defined at line 44 for '#'
+; You must use the XOR_bytes function for exclusive or!
+            PUSH {LR, R4-R8}
 
-    ; Use EOM defined at line 44 for '#'
+            MOV R4, R0          ;R4 - original message address
+            MOV R5, R1          ;R5 - key
+            MOV R6, R2          ;R6 - enc address
+            LDRB R7, EOM        ;R7 - '#'
 
+encLoop     LDRB R8, [R4], #1   ;R8 - byte of original message
+            MOV R0, R8          ;R0 - org msg byte
+            MOV R1, R5          ;R1 - key
+            BL XOR_bytes        ; XOR_Bytes / R0 - enc message bytes
 
+            STRB R0, [R6], #1   ;store mesge bytes in memory and incriment R2 address
+            CMP R8, R7           ;compare msg byte and "#"
+            BEQ end              ;exit subroutine if end of message
+            B encLoop           ;go back through loop
 
-
-    ; You must use the XOR_bytes function for exclusive or!
-    BL XOR_bytes        ; XOR_Bytes
-
-
-
-
-
+end         POP{LR, R4-R8}
+            BX LR
 ; =============== End of your code ================================
     .endasmfunc
 
@@ -90,19 +97,28 @@ Encrypt:   .asmfunc
 ; Return:
 ;   None
 Decrypt:    .asmfunc
-; ============ Add your code and comments below ===================
+; ============ Add your code and comments below =================== ; Use EOM defined at line 44 for '#'
+; You must use the XOR_bytes function for exclusive or!
+            PUSH {LR, R4-R8}
 
-    ; Use EOM defined at line 44 for '#'
+            MOV R4, R0          ;R4 - enc msg address
+            MOV R5, R1          ;R5 - key
+            MOV R6, R2          ;R6 - dec msg address
+            LDRB R7, EOM        ;R7 - '#'
+
+DecLoop     LDRB R8, [R4], #1   ;R8 - byte of enc message
+            MOV R0, R8          ;R0 - enc msg byte
+            MOV R1, R5          ;R1 - key
+            BL XOR_bytes        ; XOR_Bytes / R0 - message bytes
+
+            STRB R0, [R6], #1    ;store mesge bytes in memory and incriment R2 address
+            CMP R0, R7           ;compare msg byte and "#"
+            BEQ decEnd           ;exit subroutine if end of message
+            B DecLoop            ;go back through loop
 
 
-
-
-    ; You must use the XOR_bytes function for exclusive or!
-    BL XOR_bytes        ; XOR_Bytes
-
-
-
-
+decEnd      POP {LR, R4-R8}
+            BX LR
 ; =============== End of your code ================================
     .endasmfunc
     .end
